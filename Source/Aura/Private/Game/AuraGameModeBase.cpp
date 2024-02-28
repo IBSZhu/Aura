9ba2bb08b52d4eed57e6d946c3bfd641b17a5769
IBSZhu <1246912921@qq.com>
2024-02-28 19:15:09 +0800

    section1.9-2.1

---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// IBSZhu 
+
+
+#include "Game/AuraGameModeBase.h"
+
--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,10 +4,92 @@
 #include "Player/AuraPlayerController.h"
 
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility,false,CursorHit);
+	if(!CursorHit.bBlockingHit) return;//未命中
+
+	LastActor = ThisActor;
+	ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+
+	/**
+	 * Line trace from cursor. there are several scenarios:
+	 * A. LastActor is null & ThisActor is null
+	 *		-Do nothing.
+	 * B. LastActor is null & ThisActor is valid
+	 *		-Highlight ThisActor.
+	 * C. LastActor is valid & ThisActor is null
+	 *		-UnHighlight LastActor.
+	 * D. Both Actors are valid, but LastActor != ThisActor
+	 *		-Highlight ThisActor & UnHighlight LastActor.
+	 * E. Both Actors are valid, and LastActor = ThisActor
+	 *		-Do nothing.
+	 */
+
+	/*if(LastActor == nullptr && ThisActor == nullptr)
+	{
+		
+	}
+	else if(LastActor == nullptr && ThisActor != nullptr)
+	{
+		ThisActor->HighlightActor();
+	}
+	else if(LastActor != nullptr && ThisActor == nullptr)
+	{
+		LastActor->UnHighlightActor();
+	}
+	else if(LastActor != nullptr && ThisActor != nullptr && LastActor != ThisActor)
+	{
+		ThisActor->HighlightActor();
+		LastActor->UnHighlightActor();
+	}
+	else if(LastActor != nullptr && ThisActor != nullptr && LastActor != ThisActor)
+	{
+		
+	}*/
+	if(LastActor == nullptr)
+	{
+		if(ThisActor != nullptr)
+		{
+			//case B
+			ThisActor->HighlightActor();
+		}
+		//case A, do nothing
+	}
+	else //LastActor is valid
+	{
+		if(ThisActor == nullptr)
+		{
+			//case C
+			LastActor->UnHighlightActor();
+		}
+		else //Both are valid
+		{
+			if(LastActor != ThisActor)
+			{
+				//case D
+				ThisActor->HighlightActor();
+				LastActor->UnHighlightActor();
+			}
+			//case E, do nothing
+		}
+	}
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -27,3 +109,31 @@ void AAuraPlayerController::BeginPlay()
 	InputModeData.SetHideCursorDuringCapture(false);
 	SetInputMode(InputModeData);
 }
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent =CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction,ETriggerEvent::Triggered,this,&AAuraPlayerController::Move);
+	
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2d InputAxisVector = InputActionValue.Get<FVector2d>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f,Rotation.Yaw,0.f);
+	
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if(APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection,InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection,InputAxisVector.X); 
+	}
+}
+
+
